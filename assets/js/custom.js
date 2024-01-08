7b6e6bc73f75164d83c330ed7b52cbc879889207
mdshakil24 <shakilshd23@gmail.com>
2024-01-08 16:44:35 +0600

    Done the All pages

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,20 +1,21 @@
 $(document).ready(function(){
 
- // Humber Menu Icon Js
- $('.all-p-humber').click(function(){
-    $(this).toggleClass('open');
-  });
-      // Humber Menu Icon Js
+
+// Humber Menu Icon Js
   $('.mobile-menu-icon').click(function(){
-    $('.im-sidebar-area').toggleClass('show');
+    $('.im-sidebar-area').addClass('show');
   });
-  $('.mobile-menu-icon').click(function(){
-    $(body).addClass('darken');
+  $('.mobile-menu-close-icon').click(function(){
+    $('.im-sidebar-area').removeClass('show');
   });
 
-   
       
 })
+
+let fullBody = document.body;
+function bodyClass() {
+    fullBody.classList.toggle('overlay');
+}
 
 
 
