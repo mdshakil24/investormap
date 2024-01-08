9a6e22198fc617138419f0a307e2dc70ecb29b7f
mdshakil24 <shakilshd23@gmail.com>
2024-01-08 12:50:00 +0600

    Mobile version with some changes

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,5 +1,17 @@
 $(document).ready(function(){
 
+ // Humber Menu Icon Js
+ $('.all-p-humber').click(function(){
+    $(this).toggleClass('open');
+  });
+      // Humber Menu Icon Js
+  $('.mobile-menu-icon').click(function(){
+    $('.im-sidebar-area').toggleClass('show');
+  });
+  $('.mobile-menu-icon').click(function(){
+    $(body).addClass('darken');
+  });
+
    
       
 })
